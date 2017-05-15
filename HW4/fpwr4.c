42d09538f67a8269fc5774387653df2335582333
Andrew Grove <grovea@lnxsrv09.seas.ucla.edu>
2017-05-14 22:57:17 -0700

    05/14/2017

--- HW4/fpwr4.c
+++ HW4/fpwr4.c
@@ -1,41 +1,44 @@
 #include <stdio.h>
 
 static float u2f(unsigned u) {
-	union convert {
-		unsigned i;
-		float f;
-	} c;
+  union convert {
+    unsigned i;
+    float f;
+  } c;
 
-	c.i = u;
-	return c.f;
+  c.i = u;
+  return c.f;
 }
 
 float fpwr4(int x) {
-	unsigned exp, frac;
-	unsigned u;
+  unsigned exp, frac;
+  unsigned u;
 
-	if (x < -74) {
-		exp = 0;
-		frac = 0;
-	}
-	else if (x < -63) {
-		exp = 0;
-		frac = 1 << (x*2 + 149);
-	}
-	else if (x < 64) {
-		exp = x + x*2;
-		frac = 0
-	}
-	else {
-		exp = 255;
-		frac = 0;
-	}
+  if (x < -74) {
+    exp = 0;
+    frac = 0;
+  }
+  else if (x < -63) {
+    exp = 0;
+    frac = 1 << (x*2 + 149);
+  }
+  else if (x < 64) {
+    exp = x*2 + 127;
+    frac = 0;
+  }
+  else {
+    exp = 255;
+    frac = 0;
+  }
 
-	u = exp << 23 | frac;
-	return u2f(u);
+  printf("%i\n", exp);
+  printf("%x\n", frac);
+  printf("%i\n", u);
+
+  u = exp << 23 | frac;
+  return u2f(u);
 }
 
 int main() {
-	printf("%f", fpwr4(2));
-	printf("%f", fpwr4(3));
+  printf("%.100f\n", fpwr4(-65));
 }
--- HW4/test.c
+++ HW4/test.c
@@ -5,30 +5,32 @@
 
 int main() {
   srand(time(NULL));
-  
-  int x = rand();
-  int y = rand();
-  int z = rand();
 
-  double dx = (double)x;
-  double dy = (double)y;
-  double dz = (double)z;
+  int x;
+  int y;
+  int z;
+
+  double dx;
+  double dy;
+  double dz;
   
-  for (int i = 0; i < 50000; i++) {
-    x = INT_MAX - 32428972;
-    y = rand();
-    z = rand();
+  for (int i = 0; i < 20; i++) {
+    x = rand();
+    y = 234;
+    z = rand();;
 
     dx = (double)x;
     dy = (double)y;
     dz = (double)z;
-  
-    if ((float)x != (float)dx) {
-      printf("%i", x);
+
+    if (dx/dx != dz/dz) {
+      printf("%f\n", dx);
+      printf("%f\n", dy);
+      printf("%f\n", dz);
       return 1;
     }
   }
-  
+
   printf("No Issues!\n");
   return 0;
 }
